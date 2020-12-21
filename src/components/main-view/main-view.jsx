09e42a2b75d714c3d1f37c4be2521aa0c0c7ed55
ximnoise <mrogge96@protonmail.com>
2020-12-21 15:25:50 +0100

    add functionality for displaying movie list from api

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import axios from 'axios';
+
+
+export class MainView extends React.Component {
+  constructor() {
+    // Call the superclass constructor
+    // so React can initialize it
+    super();
+
+    // Initialize the state to an empty object so we can destructure it later
+    this.state = {};
+  }
+
+  componentDidMount() {
+    axios.get('https://primedome.herokuapp.com/movies')
+      .then(response => {
+        // Assign the result to the state
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  // This overrides the render() method of the superclass
+  // No need to call super() though, as it does nothing by default
+  render() {
+    // If the state isn't initialized, this will throw on runtime
+    // before the data is initially loaded
+    const { movies } = this.state;
+
+    // Before the movies have been loaded
+    if (!movies) return <div className="main-view"/>;
+
+    return (
+      <div className="main-view">
+        { movies.map(movie => (
+          <div className="movie-card" key={movie._id}>{movie.Title}</div>
+        ))}
+      </div>
+    );
+  }
+}
