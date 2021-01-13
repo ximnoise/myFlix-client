2bd24255c521ed35a6228b482431bfef56d72d8e
ximnoise <mrogge96@protonmail.com>
2021-01-13 16:54:49 +0100

    update store variable

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 
 import { MainView } from './components/main-view/main-view';
 import moviesApp from './reducers/reducers';
 
 import './index.scss';
 
-const store = createStore(moviesApp);
+const store = createStore(moviesApp, devToolsEnhancer());
 
 
 // Main component (will eventually use all the others)
